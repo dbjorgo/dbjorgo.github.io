eab78497cdbe45f586f1ee20b7981972c18f7212
dbjorgo <daniel.bjorgo@gmail.com>
2024-09-16 08:30:14 +0200

    update aboutus

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,7 +4,7 @@
       <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div class="flex lg:flex-1">
           <router-link to="/" class="-m-1.5 p-1.5">
-            <span class="sr-only">Gatonegro</span>
+            <span class="sr-only">Winenot</span>
             <img class="h-8 w-auto" src=/wineHome.png alt="" />
           </router-link>
         </div>
@@ -25,7 +25,7 @@
         <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div class="flex items-center justify-between" >
             <a href="/" class="-m-1.5 p-1.5">
-              <span class="sr-only">Gatonegro</span>
+              <span class="sr-only">Winenot</span>
               <img class="h-8 w-auto" src=/wineHome.png alt="" />
             </a>
             <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen1 = false" >
